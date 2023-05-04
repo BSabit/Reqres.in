e651141cf3080539ea2a01c2346edb8c4bbceb6e
ASUS <batyrkhanov95@gmail.com>
2023-05-05 04:38:40 +0600

    Изменение в контроллер 3

--- src/main/java/ru/batyrkhanov/springcourse/PrognozPogody/PrognozController.java
+++ src/main/java/ru/batyrkhanov/springcourse/PrognozPogody/PrognozController.java
@@ -9,52 +9,26 @@ import ru.batyrkhanov.springcourse.PrognozPogody.model.Root;
 @RequestMapping("/prognoz")
 public class PrognozController {
 
-    @GetMapping("/astana")
-    public Response findAstana() {
-        return RestTemp("ASTANA");
+    @GetMapping("/{city}")
+    public Response findAstana(@PathVariable("city") String city) {
+        return RestTemp(city);
     }
 
-
-    @GetMapping("/almaty")
-    public Response findAlmaty() {
-       return RestTemp("ALMATY");
-    }
-
-    @GetMapping("/aktau")
-    public Response findAktau() {
-       return RestTemp("AKTAU");
-    }
-
-    @GetMapping("/shymkent")
-    public Response findShymkent() {
-        return RestTemp("SHYMKENT");
-    }
-
-
-
-    @GetMapping("/astana/{id}")
-     public Root findAstanaId(@PathVariable("id") int id) {
-        return RestTemp2(id);
+    @GetMapping("/{city}/{id}")
+     public Root findAstanaId(@PathVariable("city") String city, @PathVariable("id") int id) {
+        return RestTemp2(city, id);
         }
-
-    @GetMapping("/almaty/{id}")
-    public Root findAlmatyId(@PathVariable("id") int id) {
-        return RestTemp2(id);
-    }
-
-    @GetMapping("/aktau/{id}")
-    public Root findAktauId(@PathVariable("id") int id) {
-        return RestTemp2(id);
-    }
-
-    @GetMapping("/shymkent/{id}")
-    public Root findShymkentId(@PathVariable("id") int id) {
-        return RestTemp2(id);
-    }
 
 
     public Response RestTemp(String http) {
         RestTemplate restTemplate = new RestTemplate();
+        switch (http) {
+            case "astana" -> http = "ASTANA";
+            case "almaty" -> http = "ALMATY";
+            case "aktau" -> http = "AKTAU";
+            case "shymkent" -> http = "SHYMKENT";
+            default -> http = "SEMEI";
+        }
         Root root = restTemplate.getForObject("http://api.weatherapi.com/v1/current.json?key=c33f9d7ea0ef4ce6b19161711230405&q="
                 + http, Root.class);
         assert root != null;
@@ -62,10 +36,17 @@ public class PrognozController {
                 root.getLocation().getLocaltime(), root.getCurrent().getTemp_c());
     }
 
-    public Root RestTemp2(int http) {
+    public Root RestTemp2(String http, int id) {
         RestTemplate restTemplate = new RestTemplate();
+        switch (http) {
+            case "astana" -> http = "ASTANA";
+            case "almaty" -> http = "ALMATY";
+            case "aktau" -> http = "AKTAU";
+            case "shymkent" -> http = "SHYMKENT";
+            default -> http = "SEMEI";
+        }
         return restTemplate.getForObject("http://api.weatherapi.com/v1/forecast.json?key=c33f9d7ea0ef4ce6b19161711230405&q=Astana&days="
-                + http, Root.class);
+                + id, Root.class);
     }
     }
 
