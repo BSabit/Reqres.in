5813727645a2b3ea027d24b29e46db2de8e8f14b
ASUS <batyrkhanov95@gmail.com>
2023-05-05 04:01:41 +0600

    Изменение в контроллере 2

--- src/main/java/ru/batyrkhanov/springcourse/PrognozPogody/PrognozController.java
+++ src/main/java/ru/batyrkhanov/springcourse/PrognozPogody/PrognozController.java
@@ -11,59 +11,61 @@ public class PrognozController {
 
     @GetMapping("/astana")
     public Response findAstana() {
-        return RestTemp("http://api.weatherapi.com/v1/current.json?key=c33f9d7ea0ef4ce6b19161711230405&q=ASTANA");
+        return RestTemp("ASTANA");
     }
 
 
     @GetMapping("/almaty")
     public Response findAlmaty() {
-       return RestTemp("http://api.weatherapi.com/v1/current.json?key=c33f9d7ea0ef4ce6b19161711230405&q=ALMATY");
+       return RestTemp("ALMATY");
     }
 
     @GetMapping("/aktau")
     public Response findAktau() {
-       return RestTemp("http://api.weatherapi.com/v1/current.json?key=c33f9d7ea0ef4ce6b19161711230405&q=AKTAU");
+       return RestTemp("AKTAU");
     }
 
     @GetMapping("/shymkent")
     public Response findShymkent() {
-        return RestTemp("http://api.weatherapi.com/v1/current.json?key=c33f9d7ea0ef4ce6b19161711230405&q=SHYMKENT");
+        return RestTemp("SHYMKENT");
     }
 
 
 
     @GetMapping("/astana/{id}")
      public Root findAstanaId(@PathVariable("id") int id) {
-        return RestTemp2("http://api.weatherapi.com/v1/forecast.json?key=c33f9d7ea0ef4ce6b19161711230405&q=Astana&days=" + id);
+        return RestTemp2(id);
         }
 
     @GetMapping("/almaty/{id}")
     public Root findAlmatyId(@PathVariable("id") int id) {
-        return RestTemp2("http://api.weatherapi.com/v1/forecast.json?key=c33f9d7ea0ef4ce6b19161711230405&q=Almaty&days=" + id);
+        return RestTemp2(id);
     }
 
     @GetMapping("/aktau/{id}")
     public Root findAktauId(@PathVariable("id") int id) {
-        return RestTemp2("http://api.weatherapi.com/v1/forecast.json?key=c33f9d7ea0ef4ce6b19161711230405&q=Aktau&days=" + id);
+        return RestTemp2(id);
     }
 
     @GetMapping("/shymkent/{id}")
     public Root findShymkentId(@PathVariable("id") int id) {
-        return RestTemp2("http://api.weatherapi.com/v1/forecast.json?key=c33f9d7ea0ef4ce6b19161711230405&q=Shymkent&days=" + id);
+        return RestTemp2(id);
     }
 
 
     public Response RestTemp(String http) {
         RestTemplate restTemplate = new RestTemplate();
-        Root root = restTemplate.getForObject(http, Root.class);
+        Root root = restTemplate.getForObject("http://api.weatherapi.com/v1/current.json?key=c33f9d7ea0ef4ce6b19161711230405&q="
+                + http, Root.class);
         assert root != null;
         return new Response(root.getLocation().getCountry(), root.getLocation().getName(),
                 root.getLocation().getLocaltime(), root.getCurrent().getTemp_c());
     }
 
-    public Root RestTemp2(String http) {
+    public Root RestTemp2(int http) {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(http, Root.class);
+        return restTemplate.getForObject("http://api.weatherapi.com/v1/forecast.json?key=c33f9d7ea0ef4ce6b19161711230405&q=Astana&days="
+                + http, Root.class);
     }
     }
 
