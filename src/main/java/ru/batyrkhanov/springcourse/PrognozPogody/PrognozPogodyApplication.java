dfa19453f99f4aeb38cfd271914988df1cbd8224
Nuras) <batyrkhanov.nuras@gmail.com>
2023-05-08 15:15:29 +0600

    exclude DB connection

--- src/main/java/ru/batyrkhanov/springcourse/PrognozPogody/PrognozPogodyApplication.java
+++ src/main/java/ru/batyrkhanov/springcourse/PrognozPogody/PrognozPogodyApplication.java
@@ -2,8 +2,9 @@ package ru.batyrkhanov.springcourse.PrognozPogody;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PrognozPogodyApplication {
 
 	public static void main(String[] args) {
